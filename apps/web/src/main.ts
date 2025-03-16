ccd83b2f142740ebf50e965fb195dc8c2b721f19
Marcin Makarewicz <marcin@makarewicz.eu>
2025-03-16 11:21:00 +0100

    feat(web): teachable machine image model

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,13 +1,14 @@
 import { Header } from "@repo/ui/header";
 import "./style.css";
 import typescriptLogo from "/typescript.svg";
+import viteLogo from "/vite.svg";
 import { Counter } from "@repo/ui/counter";
 import { setupCounter } from "@repo/ui/setup-counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="./vite.svg" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
